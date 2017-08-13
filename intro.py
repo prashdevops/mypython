f5f06e0cbc2b9657dbb155b08aa74ccd524f7137
prash.devops@gmail.com <root@localhost.localdomain>
2017-08-12 23:18:54 -0400

    modified with some custom variable example

--- intro.py
+++ intro.py
@@ -1,3 +1,12 @@
 # This is my first python program and This is Intrduction
 # Developed by Prash.DevOps
-print('Hello Word')
+my_message = 'Hello Word'
+my_custom_message = 'Prash\'s Custom Message'
+my_custom_message_one = "Prash's Second Message"
+my_custom_message_multiline = """ Hi , Here is my example of custom variables
+   which is having multilines """
+print(my_message)
+print(my_custom_message)
+print(my_custom_message_one)
+print(my_custom_message_multiline)
+
